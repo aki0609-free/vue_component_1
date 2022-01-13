8631d785db7bbb5157284a7ade110a70fac9685f
aki0609-free <akiyamafree0609@gmail.com>
2022-01-13 17:01:32 +0900

    section08_start

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,103 @@
 <template>
-  <section>
-    <h2>My Friends</h2>
-    <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
-    </ul>
-  </section>
+  <div>
+    <active-element
+      :topic-title="activeTopic && activeTopic.title"
+      :text="activeTopic && activeTopic.fullText"
+    ></active-element>
+    <knowledge-base
+      :topics="topics"
+      @select-topic="activateTopic"
+    ></knowledge-base>
+  </div>
 </template>
 
 <script>
-import FriendContact from './components/FriendContact.vue';
 export default {
-  components: { FriendContact },
   data() {
     return {
-      frinds: [
+      topics: [
         {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
+          id: "basics",
+          title: "The Basics",
+          description: "Core Vue Basics you have to know",
+          fullText:
+            "Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!",
         },
         {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "3210 4568 90",
-          email: "julie@localhost.com",
+          id: "components",
+          title: "Components",
+          description:
+            "Components are a core concept for building Vue UIs and apps",
+          fullText:
+            "With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.",
         },
       ],
+      activeTopic: null,
     };
+  },
+  methods: {
+    activateTopic(topicId) {
+      this.activeTopic = this.topics.find((topic) => topic.id === topicId);
+    },
   },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+section {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 2rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+li {
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  width: 15rem;
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h2 {
+  margin: 0.75rem 0;
+  text-align: center;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #c70053;
+  background-color: #c70053;
+  color: white;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #e24d8b;
+  border-color: #e24d8b;
+}
+</style>
