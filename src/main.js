7f74b8755a3c119333f5579ffabd889c4aad63ac
aki0609-free <akiyamafree0609@gmail.com>
2022-01-20 09:31:17 +0900

    section10_init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import { createApp } from 'vue';
 
-import App from './App.vue';
-import FriendContact from "./components/FriendContact.vue";
-
-const app = createApp(App);
-
-app.component('friend-contact',FriendContact);
-
-app.mount('#app');
+createApp({}).mount('#app');
