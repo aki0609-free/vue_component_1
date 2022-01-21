b54069a1d464ed99aa0a2628ca5e0f69718fbb5d
aki0609-free <akiyamafree0609@gmail.com>
2022-01-21 17:50:32 +0900

    section12_setup

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
-  <section>
-    <h2>My Friends</h2>
-    <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
-    </ul>
-  </section>
+  <learning-survey @survey-submit="storeSurvey"></learning-survey>
+  <user-experiences :results="savedSurveyResults"></user-experiences>
 </template>
 
 <script>
-import FriendContact from './components/FriendContact.vue';
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+
 export default {
-  components: { FriendContact },
+  components: {
+    LearningSurvey,
+    UserExperiences,
+  },
   data() {
     return {
-      frinds: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "3210 4568 90",
-          email: "julie@localhost.com",
-        },
-      ],
+      savedSurveyResults: [],
     };
+  },
+  methods: {
+    storeSurvey(surveyData) {
+      const surveyResult = {
+        name: surveyData.userName,
+        rating: surveyData.rating,
+        id: new Date().toISOString(),
+      };
+      this.savedSurveyResults.push(surveyResult);
+      console.log(surveyResult);
+    },
   },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
