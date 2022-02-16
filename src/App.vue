0f80cb2e4331e013af7490dd00ff6df3899fe88f
aki0609-free <akiyamafree0609@gmail.com>
2022-02-16 20:28:51 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,32 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 import FriendContact from './components/FriendContact.vue';
+
 export default {
   components: { FriendContact },
-  data() {
+  setup() {
+    const friends = reactive([
+      {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com"
+      },
+      {
+        id: "julie",
+        name: "Julie Jones",
+        phone: "3210 4568 90",
+        email: "julie@localhost.com"
+      }
+    ]);
+
     return {
-      frinds: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "3210 4568 90",
-          email: "julie@localhost.com",
-        },
-      ],
-    };
-  },
+      friends
+    }
+  }
 };
 </script>
 
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,31 +1,39 @@
 <template>
-    <li>
-        <h2>{{ friend.name }}</h2>
-        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
-        <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong>{{ friend.phone }}</li>
-            <li><strong>Email:</strong>{{ friend.email }}</li>
-        </ul>
-    </li>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    {{ detailsAreVisible }}
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
+import { reactive, ref } from "vue";
+
 export default {
-    data() {
-        return {
-            detailsAreVisible: false,
-            friend: {
-                id: "manuel",
-                name: "Manuel Lorenz",
-                phone: "0123 45678 90",
-                email: "manuel@localhost.com"
-            },
-        }
-    },
-    methods: {
-        toggleDetails() {
-            this.detailsAreVisible = !this.detailsAreVisible;
-        }
-    }
+  setup() {
+    let detailsAreVisible = ref(false);
+    const friend = reactive({
+      id: "manuel",
+      name: "Manuel Lorenz",
+      phone: "0123 45678 90",
+      email: "manuel@localhost.com",
+    });
+
+    const toggleDetails = () => {
+      detailsAreVisible.value = !detailsAreVisible.value;
+    };
+
+    return {
+      detailsAreVisible,
+      friend,
+      toggleDetails,
+    };
+  },
 };
 </script>
